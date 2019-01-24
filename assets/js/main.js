052c63234fd6d8bd435e406ec710b09d98a5500d
Marek <power88w@gmail.com>
2019-01-24 01:37:47 +0000

    I've connected the power on and strict buttons with .js. plus I've been working on the functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,34 @@
 
+
+var strict = false;
+
+var strictButton = document.querySelector("#strict_button")
+
+strictButton.addEventListener('click', function strictMe(event)
+{
+    if(strictButton.checked == true) {
+        strict = true;
+        console.log("ok");
+    } else {
+        strict = false;
+        console.log("off");
+    }
+})
+
+var power = false;
+var powerButton = document.querySelector("#power_button")
+var counterOn = document.querySelector("#count")
+
+powerButton.addEventListener('click', function powerMe(event) {
+    if (powerButton.checked == true) {
+        power = true;
+        counterOn.innerHTML = "--";
+        console.log("ok");
+    } else  {
+        power = false;
+        counterOn.innerHTML = "";
+        console.log("Off");
+    }
+})
+
+
