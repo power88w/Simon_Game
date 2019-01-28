ce4828db6b2c5dbeae82cf69a65f97eec158435a
Marek <power88w@gmail.com>
2019-01-28 02:23:00 +0000

    Addition of the start game function and attempts to connect the sound and improve the colors

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 
+//-- strict mode --/
 
 var strict = false;
 
@@ -13,22 +14,65 @@ strictButton.addEventListener('click', function strictMe(event)
         strict = false;
         console.log("off");
     }
-})
+});
 
+//---power---/
 var power = false;
 var powerButton = document.querySelector("#power_button")
 var counterOn = document.querySelector("#count")
+let timeStamp;
+
+
 
 powerButton.addEventListener('click', function powerMe(event) {
     if (powerButton.checked == true) {
         power = true;
-        counterOn.innerHTML = "--";
-        console.log("ok");
+        counterOn.innerHTML = "0";
+        startGame();
     } else  {
         power = false;
-        counterOn.innerHTML = "";
-        console.log("Off");
+        counterOn.innerHTML = "--";
+        //--clearColor();    // czyszczenie kolorow-----/
+        clearInterval(timeStamp);  //---ustawianie intervalow ----/
     }
-})
+});
+
+// ---   if (on || win)
+//---- play();
+
+var simonOrder = [];
+var playerOrder = [];
+var brightenColor;
+var turn;
+var sukcess;
+
+function startGame() {
+    win = false;
+    order = [];
+    playerOrder = [];
+    brightenColor = 0;
+    timeStamp = 0;
+    turn = 1;
+    counterOn.innerHTML = 1;
+    sukcess = true;
+    for (var i = 0; i < 20; i++) {
+        var rand = Math.random();
+        rand4 = rand * 4;
+        order.push(Math.floor(rand4)+1);
+    }
+    simonOrder = true;
+    timeStamp = setInterval(gameTurn, 800);
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
